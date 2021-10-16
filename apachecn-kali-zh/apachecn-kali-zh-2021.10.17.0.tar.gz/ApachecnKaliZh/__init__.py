34ba97f6c1f142b9349d11498d0ad5165274fc1f
Tom Forbes <tom@tomforb.es>
2021-10-16 16:40:34 +0000

    {"name":"apachecn-kali-zh","version":"2021.10.17.0","file":"apachecn-kali-zh-2021.10.17.0.tar.gz","path":"apachecn-kali-zh/apachecn-kali-zh-2021.10.17.0.tar.gz"}

---
+++ apachecn-kali-zh/apachecn-kali-zh-2021.10.17.0.tar.gz/ApachecnKaliZh/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-kali-zh
+https://github.com/apachecn/apachecn-kali-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.10.17.0"
